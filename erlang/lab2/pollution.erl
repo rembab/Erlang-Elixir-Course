0ca2b746c5d19e6ff7027bf71902bd7c8fffba01
Remigiusz Babiarz <rbabiarz@student.agh.edu.pl>
2026-04-02 23:26:37 +0200

    almost finished work at pollution

--- erlang/lab2/pollution.erl
+++ erlang/lab2/pollution.erl
@@ -1,5 +1,5 @@
 -module(pollution).
--export([create_monitor/0, add_station/3, add_value/5, remove_value/4, get_one_value/4]).
+-export([create_monitor/0, add_station/3, add_value/5, remove_value/4, get_one_value/4, get_station_min/3, get_station_mean/3, get_daily_mean/3]).
 
 -record(reading, {dateTime, type, value}).
 -record(station, {name, coords, readings=[]}).
@@ -9,7 +9,7 @@ create_monitor() ->
 
 has_station(NameOrCoords, Monitor) ->
   case maps:find(NameOrCoords, Monitor) of
-    {error, _} -> 
+    error -> 
       false;
     {ok, _} ->
       true
@@ -17,23 +17,20 @@ has_station(NameOrCoords, Monitor) ->
 
 get_reading(DateTime, Type, Station) ->
   case [R || R <- Station#station.readings, R#reading.type =:= Type, R#reading.dateTime =:= DateTime] of
-    [R] -> 
-      {true, R};
+    [A] -> 
+      {true, A};
     [] ->
       false
   end.
-
 
 add_station(Name, Coords, Monitor) ->
   case has_station(Name, Monitor) or has_station(Coords, Monitor) of
     false ->
       Station = #station{coords = Coords, name = Name},
-      Monitor#{Name := Station, Coords := Station};
+      Monitor#{Name => Station, Coords => Station};
     true ->
       {error, "Station with matching coordinates or name already exists"}
   end.
-
-
 
 add_value(NameOrCoords, DateTime, Type, Value, Monitor) ->
   case has_station(NameOrCoords, Monitor) of  
@@ -45,15 +42,18 @@ add_value(NameOrCoords, DateTime, Type, Value, Monitor) ->
         {true, _} ->
           {error, "A reading value with same time and type already exists for this station"};
         false ->
-          NewReadings = Station#station.readings ++ #reading{type = Type, value = Value, dateTime = DateTime},
-          Monitor#{Station := Station#station{readings = NewReadings}}
+          NewReadings = [#reading{type = Type, value = Value, dateTime = DateTime} | Station#station.readings],
+          NewStation = Station#station{readings = NewReadings},
+          Name = NewStation#station.name,
+          Coords = NewStation#station.coords,
+          Monitor#{Name := NewStation, Coords := NewStation}
       end
   end.
-
 
 remove_value(NameOrCoords, DateTime, Type, Monitor) ->
   case has_station(NameOrCoords, Monitor) of  
     false ->
+      io:format("f1\n"),
       {error, "No station with such name or coordinates"};
     true ->
       #{NameOrCoords := Station} = Monitor, 
@@ -61,33 +61,69 @@ remove_value(NameOrCoords, DateTime, Type, Monitor) ->
         false ->
           {error, "A reading value at this time and of this type does not exists in this station's readings"};
         {true, R} ->
-          NewReadings = lists:filter(fun (R) -> true; (_) -> false end, Station#station.readings),
+          NewReadings = lists:filter(fun (X) when X =:= R -> false; (_) -> true end, Station#station.readings),
+          NewStation = Station#station{readings = NewReadings},
+          Name = NewStation#station.name,
+          Coords = NewStation#station.coords,
 
-          Monitor#{Station := Station#station{readings = NewReadings}}
+          Monitor#{Name := NewStation, Coords := NewStation}
       end
   end.
 
-
-get_one_value(NameOrCoords, Type, DateTime, Monitor) ->
+get_one_value(NameOrCoords, DateTime, Type, Monitor) ->
   case has_station(NameOrCoords, Monitor) of  
     false ->
       {error, "No station with such name or coordinates"};
     true ->
       #{NameOrCoords := Station} = Monitor, 
-      case lists:search(fun 
-                          (#reading{type = T, dateTime = DT}) when T =:= Type, DT =:= DateTime -> 
-                            true; 
-                          (_) -> 
-                        false end,
-                        Station#station.readings) of
-        {value, Value} ->
-          Value;
+      case get_reading(DateTime, Type, Station) of 
+        {true, R} ->
+          R#reading.value;
         false ->
           {error, "A reading value at this time and of this type does not exists in this station's readings"}
       end
   end.
 
-get_station_min(NameOrCoords)
+get_station_min(NameOrCoords, Type, Monitor) ->
+  case has_station(NameOrCoords, Monitor) of  
+    false ->
+      {error, "No station with such name or coordinates"};
+    true ->
+      #{NameOrCoords := Station} = Monitor, 
+      Xs = [X || X <- Station#station.readings, X#reading.type =:= Type],
+      case Xs of
+        [] ->
+          {error, "Station has no readings of aformentioned type"};
+        [Head|Tail] ->
+          lists:foldl(fun (X, Acc) -> min(X,Acc) end, Head, Tail)
+      end
+  end.
+
+get_station_mean(NameOrCoords, Type, Monitor) ->
+  case has_station(NameOrCoords, Monitor) of  
+    false ->
+      {error, "No station with such name or coordinates"};
+    true ->
+      #{NameOrCoords := Station} = Monitor, 
+      Xs = [X#reading.value || X <- Station#station.readings, X#reading.type =:= Type],
+      case Xs of
+        [] ->
+          {error, "Station has no readings of aformentioned type"};
+        [Head|Tail] ->
+          lists:foldl(fun (X, Acc) -> X + Acc end, Head, Tail) / length(Xs)
+      end
+  end.
+    
+get_daily_mean(Type, Day, Monitor) ->
+  Stations = maps:values(Monitor),
+  Readings = lists:foldl(fun (R, Acc) -> [R#station.readings] ++ Acc end, [], Stations),
+  ReadingsInDay = lists:filter(fun 
+                                 (R) when R#reading.type == Type, element(1, R#reading.dateTime) == Day
+                                          -> true; 
+                                 (_) 
+                                 -> false end, Readings),
+  io:format("~p", length(ReadingsInDay)),
+  lists:foldl(fun (X, Acc) -> X#reading.value + Acc end, 0, ReadingsInDay) / length(ReadingsInDay).
 
 
 
