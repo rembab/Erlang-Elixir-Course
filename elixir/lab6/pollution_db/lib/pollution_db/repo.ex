eee5c906c1e7dc8df82fc7f4ee538007e40ffc6e
Remigiusz Babiarz <rbabiarz@student.agh.edu.pl>
2026-06-16 08:16:38 +0200

    lab6 prog

--- elixir/lab6/pollution_db/lib/pollution_db/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule PollutionDb.Repo do
-  use Ecto.Repo,
-    otp_app: :pollution_db,
-    adapter: Ecto.Adapters.Postgres
-end
--- elixir/lab6/pollution_db/test/pollution_db_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PollutionDbTest do
-  use ExUnit.Case
-  doctest PollutionDb
-
-  test "greets the world" do
-    assert PollutionDb.hello() == :world
-  end
-end
